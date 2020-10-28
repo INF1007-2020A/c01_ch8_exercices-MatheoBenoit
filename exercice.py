f962119a7bc43e2e70d723d8f0362c8b0eb37105
MatheoBenoit <matheo.benoit-paraschivoiu@polymtl.ca>
2020-10-28 15:43:06 -0700

    manque le dernier exo

--- exercice.py
+++ exercice.py
@@ -3,6 +3,9 @@
 
 
 # TODO: Importez vos modules ici
+import json
+import string
+
 
 
 
@@ -37,9 +40,76 @@ def triplet(fichier, fichier2):
     # with open(fichier, "r") as data, open(fichier2, "w") as f2:
     #     f2.write(data.read().replace(" ", "   "))
 
+def notes():
+    correspondances = {20: "F", 40: "D", 50: "C", 70: "B", 85: "A"}
+    with open("notes.txt") as f, open("nouveau_fichier", "w") as n:
+        tableau = f.readlines()
+        for i in tableau:
+            i.replace("n/", "")
+            if float(i) < 21:
+                notes = "F"
+            elif float(i) < 41:
+                notes = "D"
+            elif float(i) < 51:
+                notes = "C"
+            elif float(i) < 71:
+                notes = "B"
+            else:
+                notes = "A"
+            n.write(i + " " + notes + "/n")
+#sinon voir screenshot 28 octobre
+
+
+
+def recette():
+    dict ={}
+    condition = True
+
+    while (condition):
+        recette = input ("ecrivez une recette, si vous avez termine, ecrivez fini")
+        if recette !="fini":
+            ingredient = input("ecrivez les ingredient de cette recette")
+            dict.update({recette: ingredient})
+        else:
+            condition = False
+
+    y = input("Que voulez vous faire dautre? A.modifier une recette, B. suppprimer une recette, C.vous avez fini")
+
+    if y == "C":
+        with open("test.json", 'w') as f:
+            json.dump(dict, f, indent=4)
+        return
+    if y =="A" or y == "B":
+        z = input("quelle recette?")
+        if y == "B":
+            del dict[z]
+        else:
+            ingredients = input("ecrire ingredient")
+            dict[z] = ingredients
+
+    else:
+        print("commande non valide")
+
+    with open("test.json", 'w') as f:
+        json.dump(dict, f, indent=4)
+
+#meilleur version voir screenshot 28 octobre
+
+def nombre(fichier):
+
+    with open(fichier) as f:
+        resultat = [int(word) for word in f.read().split() if word.isdigit()]
+    return resultat.sort
+
+#.read() donne une string de tout le texte (au lieu de readline ou reaslines que ces par ligne
+#et non tout le text. Alors quand on fait .split() on fais ensuite une liste ou chaque element sont les mots
+#puisquon separe par les espaces
+
+
+
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
+    nombre("exemple.txt")
 
-    pass
